5bac35e10a152c340e0753039cdee6faade655be
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-17 22:01:44 -0700

    Adds NSDate extension methods

---
+++ TDSwiftGraph/NSDate.swift
@@ -0,0 +1,67 @@
+//
+//  NSDate.swift
+//  TDSwiftGraph
+//
+//  Created by Tieshow Daniels on 4/17/17.
+//  Copyright © 2017 Ty Daniels. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    
+    private func dateComponents() -> DateComponents {
+        let calander = NSCalendar.current
+        let calComponents: Set<Calendar.Component> = [.second, .minute, .hour, .day, .month, .year]
+        return calander.dateComponents(calComponents, from: self as Date, to: Date())
+    }
+    
+    public var timeAgoForFeed: String {
+        let components = self.dateComponents()
+        
+        if components.month! > 0 && components.day! >= 25{
+            if components.month! < 2 {
+                return ("1 month ago")
+            } else if components.month!<=6{
+                return stringFromFormat(format: "%d months ago", withValue: components.month!)
+            } else{
+                return "a while ago"
+            }
+        }
+        
+        if components.day! >= 7 {
+            let week = components.day!/7
+            if week < 2 {
+                return ("1 week ago")
+            } else if week<4 {
+                return stringFromFormat(format: "%d weeks ago", withValue: week)
+            }
+        }
+        
+        if components.day! > 0 {
+            if components.day! < 2 {
+                return ("1 day ago")
+            } else  {
+                return stringFromFormat(format: "%d days ago", withValue: components.day!)
+            }
+        }
+        
+        if components.hour! > 0 {
+            if components.hour! < 2 {
+                return ("1 hour ago")
+            } else if components.hour!<=12 {
+                return stringFromFormat(format: "%d hours ago", withValue: components.hour!)
+            }
+        }
+        
+        if components.minute! < 2 {
+            return ("1 minute ago")
+        } else {
+            return stringFromFormat(format: "%d minutes ago", withValue: components.minute!)
+        }
+    }
+    
+    private func stringFromFormat(format: String, withValue value: Int) -> String {
+        return String(format: format, value)
+    }
+}
--- TDSwiftGraph/ViewController.swift
+++ TDSwiftGraph/ViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TDGraphViewControllerDataSource {
     
+    // MARK: - Custom Controllers
     var tdGraphNewViewController = TDGraphNewController()
+    
+    // MARK: - View data
+    var lastSection = -1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +27,35 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func dataForGraph(section: Int, completionHandler: @escaping ((x: [Double], y: [Double], y1: [Double], balance: [Double]), Bool) -> Void) {
+        
+//        if lastSection != section {
+//            cacheFired = false
+//        }
+        self.lastSection = section
+        print(section)
+        
+        //         Add some initial data
+        var xArr = [Double]()
+        var yArr = [Double]()
+        var yyArr = [Double]()
+        var balanceArr = [Double]()
+        for i in 0 ..<  40 {
+            let x = NSDate().timeAgoForFeed
+            let y = 1.2 * Double(arc4random()) / Double(UInt32.max) + 1.2
+            //            let yy = 50.0 + Double(i) * 0.05
+            let bal = Double(arc4random_uniform(UInt32(25.00))) + 2
+            xArr.append(NSDate().timeIntervalSince1970)
+            yArr.append(y)
+            yyArr.append(0.0)
+            balanceArr.append(bal)
+        }
+        
+        let dataArr = (x: xArr, y: yArr, y1: yyArr, balance: balanceArr)
+        
+        completionHandler(dataArr, false)
     }
 
 
