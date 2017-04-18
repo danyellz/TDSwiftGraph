f6a333a85a2631add2b0f23652f4bde2f7f40097
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-17 19:59:52 -0700

    Initial Commit

---
+++ TDSwiftGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDSwiftGraph
+//
+//  Created by Tieshow Daniels on 4/17/17.
+//  Copyright © 2017 Ty Daniels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
